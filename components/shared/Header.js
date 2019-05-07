abd80ae9b14b0804881092c2b9b6e79e039900fb
Francisco <francisco@velait.net>
2019-05-06 23:31:57 -0500

    added new scss styles and cleaned up functions

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,37 +1,73 @@
 import React from 'react';
 import Link from 'next/link';
 
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+} from 'reactstrap'; 
+
+const MyNavLink = (props) =>  {
+    const { route, title} = props;
+
+    return (
+        <Link href={route}>
+            <a className="nav-link">{title}</a>
+        </Link>
+    )
+}
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
     
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+      }
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
 
     render() {
 
         return (
-
-            <React.Fragment>
-            {this.props.children}
-
-            <Link href="/"><a>Index</a></Link>
-
-            <Link href="/about"><a>About</a></Link>
-
-            <Link href="/portfolios"><a>Portfolio</a></Link>
-
-            <Link href="/cv"><a>CV</a></Link>
-            
-            <Link href="/blogs"><a>blog</a></Link>
-        
-            </React.Fragment> 
-         
-            
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Francisco Miranda</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <MyNavLink route="/" title="Home" />
+                            </NavItem> 
+                            <NavItem>
+                                <MyNavLink route="/portfolios" title="Portfolio" />
+                            </NavItem> 
+                            <NavItem>
+                                <MyNavLink route="/" title="Home" />
+                            </NavItem> 
+                            <NavItem>
+                                <MyNavLink route="https://github.com/fm-tech" title="Git" />
+                            </NavItem>                 
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
         )
-           
-        
     }
-
-
-
 }
 
 export default Header; 
+            
+           
+        
+
+
+
