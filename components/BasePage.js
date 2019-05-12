5e73184a7e946b7586e8e662b689467374e43a18
Francisco <francisco@velait.net>
2019-05-12 11:05:44 -0500

    Layout change

--- components/BasePage.js
+++ components/BasePage.js
@@ -3,7 +3,7 @@ import {Container} from 'reactstrap';
 
 const BasePage = (props) => {
 
-    const { className } = props;
+    const className  = props.className || '';
 
     return(
         <div className={'base-page ${className}'}>
